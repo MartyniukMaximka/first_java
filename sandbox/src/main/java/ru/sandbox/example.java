9a80518b298c45cdb7c3e8f96a39b99d01c4dfe6
MartyniukMaximka <34189323+MartyniukMaximka@users.noreply.github.com>
2017-12-04 14:58:09 +0200

    добавлено несколько функций

--- sandbox/src/main/java/ru/sandbox/example.java
+++ sandbox/src/main/java/ru/sandbox/example.java
@@ -4,9 +4,25 @@ public class example {
 
     public static void main(String args[])
     {
-     System.out.print("Юху, у меня получилось...");
+     hello("world");
+     double l=5;
+        System.out.println("Площадь квадрата со стороной "+ l + " = "+ area(l));
 
+     double a=5, b=6;
+        System.out.println("Площадь прямоугольника со сторонами "+ a +" и "+ b + " = "+ area(a, b));
 
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Юху, у меня получилось... " + somebody);
+    }
+
+    public static double area(double l){
+        return l*l;
+    }
+
+    public static double area(double a, double b){
+        return a*b;
     }
 
 }
