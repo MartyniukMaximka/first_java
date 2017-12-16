3e27d8157c93178e49f1bee0273174c38ac32a64
MartyniukMaximka <martyniuk.maxim@gmail.com>
2017-12-16 11:34:29 +0200

    second test

---
+++ addressbook/src/test/java/ru/addressbook/GroupDeletionTest.java
@@ -0,0 +1,17 @@
+package ru.addressbook;
+
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteGroup();
+        returnToGroupPage();
+    }
+
+}
