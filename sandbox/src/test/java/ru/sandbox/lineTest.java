dd6560eecf02df5997d9c8608e05d7927a046216
MartyniukMaximka <martyniuk.maxim@gmail.com>
2017-12-26 22:24:26 +0200

    2

--- sandbox/src/test/java/ru/sandbox/lineTest.java
+++ sandbox/src/test/java/ru/sandbox/lineTest.java
@@ -8,7 +8,7 @@ import static ru.sandbox.Point.line;
 public class lineTest {
 
     @Test
-    public void Point() {
+    public void Line() {
         line(1,3,1,3);
         Assert.assertEquals(Point.a, 2.8, 0.1);
     }
