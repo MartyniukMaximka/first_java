e426c93577a7636da3eac4cb87491b4af12e6256
MartyniukMaximka <martyniuk.maxim@gmail.com>
2017-12-26 22:15:47 +0200

    3

--- sandbox/src/main/java/ru/sandbox/Point.java
+++ sandbox/src/main/java/ru/sandbox/Point.java
@@ -5,12 +5,9 @@ public class Point {
 
     public static double a;
 
-    public static double main (String args[])
+    public static double line(double x1, double x2, double y1, double y2)
     {
-        double x1=1, x2=3, y1=1, y2=3;
-       // double a;
-
-        a = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+       a = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 
         System.out.println("расстояние  между точками с координатами ("+x1 + " "+ y1+") и "+"("+x2 + " "+ y2+")" + "равно:");
         System.out.println(a);
--- sandbox/src/test/java/ru/sandbox/lineTest.java
+++ sandbox/src/test/java/ru/sandbox/lineTest.java
@@ -3,10 +3,13 @@ package ru.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static ru.sandbox.Point.line;
+
 public class lineTest {
 
     @Test
     public void Point() {
-        Assert.assertEquals(Point.a, 1.0);
+        line(1,3,1,3);
+        Assert.assertEquals(Point.a, 2.8, 0.1);
     }
 }
